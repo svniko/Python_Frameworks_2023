1cdf84027a33128f2fecbd4c7ee38a249986c5ef
svniko <ya.pepulya@gmail.com>
2023-09-21 14:05:28 +0300

    lecture-2

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
+
+pets = [
+    {'kind':'cat',
+    'name':'Tom',
+    'age':10,
+    'color': 'grey'
+    },
+    {'kind':'cat',
+    'name':'Barsik',
+    'age':5,
+    'color': 'white'
+    },
+    {'kind':'dog',
+    'name':'Spike',
+    'age':2,
+    'color': 'brown'
+    },
+]
 
 @app.route("/")
 def hello_world():
@@ -22,23 +40,6 @@ def user(name):
 #                             name=name, 
 #                             age=20)
 
-pets = [
-    {'kind':'cat',
-    'name':'Tom',
-    'age':10,
-    'color': 'grey'
-    },
-    {'kind':'cat',
-    'name':'Barsik',
-    'age':5,
-    'color': 'white'
-    },
-    {'kind':'dog',
-    'name':'Spike',
-    'age':2,
-    'color': 'brown'
-    },
-]
 
 @app.route("/users")
 def home():
@@ -53,5 +54,28 @@ def pet():
                             pets=pets,
                             title='Our pets')
 
+@app.route('/lect2')
+def lect2():
+    return render_template('lecture2.jinja')
+
+@app.route('/hello', methods=['GET', 'POST'])
+def smb():
+    if request.method == "GET":
+        return render_template('hello.jinja', name='stranger')
+    else:
+        name = request.form.get('name')
+        if not name:
+            return render_template('lecture2.jinja', flag=0)
+        return render_template('hello.jinja', name=name)
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return render_template('405.jinja', title='405 error'), 405
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.jinja', title='404 error'), 404
+    
 # if __name__ == "__main__":
 #     app.run(debug=True)
