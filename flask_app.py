8713ffb2285bd530906becb2ef5755fc218c5019
svniko <ya.pepulya@gmail.com>
2023-09-26 09:35:32 +0300

    lecture3

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, flash
+import random
+
 
 app = Flask(__name__, template_folder='templates')
+app.secret_key = '1231231secretkey or very'
+
+game = {
+    'choise': None,
+    'you_win':0,
+    'comp_win':0,
+    'round': 0
+    }
+
+
 
 pets = [
     {'kind':'cat',
@@ -58,7 +70,7 @@ def pet():
 def lect2():
     return render_template('lecture2.jinja')
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello/', methods=['GET', 'POST'])
 def smb():
     if request.method == "GET":
         return render_template('hello.jinja', name='stranger')
@@ -76,6 +88,53 @@ def method_not_allowed(e):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.jinja', title='404 error'), 404
-    
+
+
+@app.route('/start/')
+def start():
+    return render_template('rsp.jinja',
+                           title="Game", start=True )
+
+@app.route('/select/<ch>/')
+def select(ch):
+    game['choise'] = ch
+    return redirect(url_for('rsp'))
+
+@ app.route('/game/')
+def rsp():
+
+    if game['round'] < 5:
+        game['round'] += 1
+        n = random.randint(0,2)
+        # 0 -rock, 1-scissors, 2-paper
+        if game['choise'] == '0' and n == 0:
+            flash('Draw', category='warning')
+
+        elif game['choise'] == '0' and n == 1:
+            flash('You win', category='success')
+            game['you_win'] +=1
+        elif game['choise'] == '0' and n == 2:
+            flash('Comp win', category='danger')
+            game['comp_win'] +=1
+        else: 
+            pass
+
+    else:
+        if game['comp_win'] > game['you_win']:
+            flash('Total Comp win', category='danger')
+            redirect(url_for('rsp'))
+        elif game['comp_win'] == game['you_win']:
+            flash('Total Draw', category='warning')
+            redirect(url_for('rsp'))
+        else:
+            flash('Total You Win. Congrats', category='success')
+            redirect(url_for('rsp'))
+
+    return render_template('rsp.jinja',
+                           title="Game" )
+
+
+
+
 # if __name__ == "__main__":
 #     app.run(debug=True)
